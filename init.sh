834a9e3244c57e12c1b567568899872d1cbe2cf9
Casey Witt <kcwitt@gmail.com>
2020-09-21 21:39:11 +0800

    misc updates

--- init.sh
+++ init.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env sh
+set -eEuo pipefail
+set -x
 
+WORKDIR="$(realpath "$(dirname "$0")")"
 SOURCE="source"
 TARGET="target"
+SOURCEFILE="${WORKDIR}/${SOURCE}.zfs"
+TARGETFILE="${WORKDIR}/${TARGET}.zfs"
 
-[ -f "${SOURCE}.zfs" ] || truncate -s 512M "${SOURCE}.zfs"
-# printf "%szfs\n%szfs\n" "${SOURCE}" "${SOURCE}" | zpool create -O compression=on -O encryption=on -O keyformat=passphrase "${SOURCE}" "${SOURCE}.zfs"
 
-[ -f "${TARGET}.zfs" ] || truncate -s 512M "${TARGET}.zfs"
-# printf "%szfs\n%szfs\n" "${TARGET}" "${TARGET}" | zpool create -O compression=on -O encryption=on -O keyformat=passphrase "${TARGET}" "${TARGET}.zfs"
+[ -f "${SOURCEFILE}" ] || truncate -s 512M "${SOURCEFILE}"
+zpool list "${SOURCE}" > /dev/null 2>&1 \
+  || printf "%szfs\n%szfs\n" "${SOURCE}" "${SOURCE}" \
+    | zpool create -O compression=on -O encryption=on -O keyformat=passphrase "${SOURCE}" "${SOURCEFILE}"
+
+[ -f "${TARGETFILE}.zfs" ] || truncate -s 512M "${TARGETFILE}"
+zpool list "${TARGET}" > /dev/null 2>&1 \
+  || printf "%szfs\n%szfs\n" "${TARGET}" "${TARGET}" \
+    | zpool create -O compression=on -O encryption=on -O keyformat=passphrase "${TARGET}" "${TARGETFILE}"

56283f68b0ea67fb7d80f0f4a22064a94e26ac2a
Casey Witt <kcwitt@gmail.com>
2020-09-22 08:22:00 +0800

    general update

--- init.sh
+++ init.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env sh
-set -eEuo pipefail
-set -x
+set -euo
 
-WORKDIR="$(realpath "$(dirname "$0")")"
+WORKDIR="$(realpath "$(pwd)")"
 SOURCE="source"
 TARGET="target"
 SOURCEFILE="${WORKDIR}/${SOURCE}.zfs"
 TARGETFILE="${WORKDIR}/${TARGET}.zfs"
 
 
-[ -f "${SOURCEFILE}" ] || truncate -s 512M "${SOURCEFILE}"
-zpool list "${SOURCE}" > /dev/null 2>&1 \
-  || printf "%szfs\n%szfs\n" "${SOURCE}" "${SOURCE}" \
+# create source
+[ -f "${SOURCEFILE}" ] || {
+  printf "creating source file: %s\n" "${SOURCEFILE}"
+  truncate -s 512M "${SOURCEFILE}"
+}
+zpool list "${SOURCE}" > /dev/null 2>&1 || {
+  printf "creating source zpool: %s\n" "${SOURCE}"
+  printf "%szfs\n%szfs\n" "${SOURCE}" "${SOURCE}" \
     | zpool create -O compression=on -O encryption=on -O keyformat=passphrase "${SOURCE}" "${SOURCEFILE}"
+}
 
-[ -f "${TARGETFILE}.zfs" ] || truncate -s 512M "${TARGETFILE}"
-zpool list "${TARGET}" > /dev/null 2>&1 \
-  || printf "%szfs\n%szfs\n" "${TARGET}" "${TARGET}" \
+# create target
+[ -f "${TARGETFILE}" ] || {
+  printf "creating target file: %s\n" "${TARGETFILE}"
+  truncate -s 512M "${TARGETFILE}"
+}
+zpool list "${TARGET}" > /dev/null 2>&1 || {
+  printf "creating target zpool: %s\n" "${TARGET}"
+  printf "%szfs\n%szfs\n" "${TARGET}" "${TARGET}" \
     | zpool create -O compression=on -O encryption=on -O keyformat=passphrase "${TARGET}" "${TARGETFILE}"
+}
+
+# yeah - finished
+printf "\nfinished\n\n"

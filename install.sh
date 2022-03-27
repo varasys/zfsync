e1b2891a751f4da25df6d6c7565b007fc66cc36c
Casey Witt <kcwitt@gmail.com>
2022-03-26 22:22:28 -0500

    made man page source uncompressed, and updated install file to gzip it during installation

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -eu
+set -u
 
 echo "DESTDIR=${DESTDIR:=""}"
 echo "PREFIX=${PREFIX:="/usr/local"}"
@@ -16,21 +16,22 @@ case "${1:-"install"}" in
     install -v -d "${DOCDIR}/zfsync"
     install -v -d "${MANDIR}"
     install -v -m 755 zfsync "${BINDIR}/"
-    install -v zfsync.service "${SYSTEMDDIR}/" || SYSTEMD=false
-    install -v zfsync.timer "${SYSTEMDDIR}/" || SYSTEMD=false
+    install -v zfsync.service "${SYSTEMDDIR}/"
+    install -v zfsync.timer "${SYSTEMDDIR}/"
     install -v inittest.sh "${DOCDIR}/zfsync/"
     install -v README.md "${DOCDIR}/zfsync/"
     install -v install.sh "${DOCDIR}/zfsync/"
     install -v UNLICENSE "${DOCDIR}/zfsync/"
-    install -v zfsync.8.gz "${MANDIR}/"
+    # install -v zfsync.1 "${MANDIR}/"
+    gzip  --to-stdout zfsync.1 > "${MANDIR}/zfsync.1.gz"
     printf '\nfinished installing zfsync\n\nread the man page for configuration and use instructions\n\n'
     ;;
   uninstall)
-    rm -v "${BINDIR}/zfsync" || :
-    rm -v "${SYSTEMDDIR}/zfsync.service" || :
-    rm -v "${SYSTEMDDIR}/zfsync.timer" || :
-    rm -v "${MANDIR}/zfsync.8.gz"
-    rm -v -rf "${DOCDIR}/zfsync" || :
+    rm -v "${BINDIR}/zfsync"
+    rm -v "${SYSTEMDDIR}/zfsync.service"
+    rm -v "${SYSTEMDDIR}/zfsync.timer"
+    rm -v "${MANDIR}/zfsync.1.gz"
+    rm -v -rf "${DOCDIR}/zfsync"
     printf '\nfinished uninstalling zfsync\n\n'
     ;;
 esac

ea67032c137b07c2e78a71a137139dd08f56c7c3
Casey Witt <kcwitt@gmail.com>
2022-03-29 16:37:03 -0500

    implemented systemd services and timer

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ echo "DESTDIR=${DESTDIR:=""}"
 echo "PREFIX=${PREFIX:="/usr/local"}"
 
 echo "BINDIR=${BINDIR:="${DESTDIR}${PREFIX}/bin"}"
-echo "DOCDIR=${DOCDIR:="${DESTDIR}${PREFIX}/share/doc"}"
+echo "DOCDIR=${DOCDIR:="${DESTDIR}${PREFIX}/share/doc/zfsync"}"
 echo "MANDIR=${MANDIR:="${DESTDIR}${PREFIX}/share/man/man1"}"
+echo "ETCDIR=${ETCDIR:="${DESTDIR}/etc/zfsync"}"
 echo "SYSTEMDDIR=${SYSTEMDDIR:="${DESTDIR}/etc/systemd/system"}"
 echo "COMPLETEDIR=${COMPLETEDIR:="${DESTDIR}${PREFIX}/share/bash-completion/completions"}"
 echo
@@ -14,28 +15,37 @@ echo
 case "${1:-"install"}" in
   install)
     install -v -d "${BINDIR}"
-    install -v -d "${DOCDIR}/zfsync"
+    install -v -d "${DOCDIR}"
     install -v -d "${MANDIR}"
+    install -v -d "${ETCDIR}"
     install -v -d "${COMPLETEDIR}"
     install -v -m 755 zfsync "${BINDIR}/"
-    install -v zfsync.service "${SYSTEMDDIR}/"
-    install -v zfsync.timer "${SYSTEMDDIR}/"
-    install -v inittest.sh "${DOCDIR}/zfsync/"
-    install -v README.md "${DOCDIR}/zfsync/"
-    install -v install.sh "${DOCDIR}/zfsync/"
-    install -v UNLICENSE "${DOCDIR}/zfsync/"
+    if [ -d "${SYSTEMDDIR}" ]; then
+      install -v zfsync-snapshot.service "${SYSTEMDDIR}/"
+      install -v zfsync-mirror.service "${SYSTEMDDIR}/"
+      install -v zfsync-snapshot.timer "${SYSTEMDDIR}/"
+    fi
+    install -v inittest.sh "${DOCDIR}/"
+    install -v README.md "${DOCDIR}/"
+    install -v install.sh "${DOCDIR}/"
+    install -v UNLICENSE "${DOCDIR}/"
+    install -v zfsync.conf.sample "${ETCDIR}/"
     install -v -T zfsync-completion.bash "${COMPLETEDIR}/zfsync"
-    # install -v zfsync.1 "${MANDIR}/"
     gzip  --to-stdout zfsync.1 > "${MANDIR}/zfsync.1.gz"
+    "${BINDIR}/zfsync" configuser zfsync "${ETCDIR}"
+
     printf '\nfinished installing zfsync\n\nread the man page for configuration and use instructions\n\n'
     ;;
   uninstall)
     rm -v "${BINDIR}/zfsync"
-    rm -v "${SYSTEMDDIR}/zfsync.service"
-    rm -v "${SYSTEMDDIR}/zfsync.timer"
     rm -v "${MANDIR}/zfsync.1.gz"
-    rm -v -rf "${DOCDIR}/zfsync"
+    rm -v -rf "${DOCDIR}"
     rm -v "${COMPLETEDIR}/zfsync"
+    if [ -d "${SYSTEMDDIR}" ]; then
+      rm -v "${SYSTEMDDIR}/zfsync-snapshot.service"
+      rm -v "${SYSTEMDDIR}/zfsync-mirror.service"
+      rm -v "${SYSTEMDDIR}/zfsync-snapshot.timer"
+    fi
     printf '\nfinished uninstalling zfsync\n\n'
     ;;
 esac

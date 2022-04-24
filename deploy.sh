ce92f916d1cbc00cabe6e9fd153471585bb30748
Casey Witt <kcwitt@gmail.com>
2022-04-24 15:56:59 -0500

    updated devel.sh script with more status messages

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,19 @@ if [ "${BRANCH}" != 'devel' ]; then
   exit 1
 else
   MSG="${1:?"missing commit message"}"
+  echo "staging updated files to devel branch ..."
   git add .
+  echo "committing updates into devel branch..."
   git commit -m "${MSG}"
+  echo "pushing devel branch ..."
   git push
+  echo "checking out master branch ..."
   git checkout master
+  echo "merging devel branch into master branch ..."
   git merge devel
+  echo "pushing master branch ..."
   git push
-  get checkout devel
+  echo "checking out devel branch ..."
+  git checkout devel
   echo "finished deploying"
 fi

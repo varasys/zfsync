edcb502374f2049af083882b283cf07c729b2bbb
Casey Witt <kcwitt@gmail.com>
2022-04-25 16:38:00 -0500

    fixed bug where script did not exit with error if ssh connection failed, and tweaked deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,10 @@ if [ "${BRANCH}" != 'devel' ]; then
   echo "error: deploy script only works from the 'devel' branch"
   exit 1
 else
-  MSG="${1:?"missing commit message"}"
   echo "staging updated files to devel branch ..."
   git add .
   echo "committing updates into devel branch..."
-  git commit -m "${MSG}"
+  git commit "$@"
   echo "pushing devel branch ..."
   git push
   echo "checking out master branch ..."
